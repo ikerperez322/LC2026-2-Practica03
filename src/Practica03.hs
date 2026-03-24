684695562865af7df557a20cc22a26f8cf9b8e66
Iker Pérez <iker.perez@ciencias.unam.mx>
2026-03-24 11:21:20 -0600

    Arreglando saturación

--- src/Practica03.hs
+++ src/Practica03.hs
@@ -82,10 +82,6 @@ distr (And f11 f12) f2 = (And (distr f11 f2) (distr f12 f2))
 distr (Or f11 f12) f2 = (Or (Or f11 f12) f2)
 distr f1 (Or f21 f22) = (Or f1 (Or f21 f22))
 
--- (Or (Or (Var "p") (Var "q")) (And (Not (Var "p")) (Not (Var "q"))))
-
--- (And (Or (Or (Var "p") (Var "q"))(And (Not (Var "p")) (Not (Var "q")))) (Or (Or (Var "p") (Var "q")) (And (Not (Var "p")) (Not (Var "q")))))
-
 {-
 RESOLUCION BINARIA
 -}
@@ -98,8 +94,6 @@ type Clausula = [Literal]
 clausulas :: Prop -> [Clausula]
 clausulas (Cons a) = [[(Cons a)]]
 clausulas (Var p) = [[(Var p)]]
--- clausulas (Or (Var p) (Var q)) = if (Var p) == (Var q) then [[(Var p)]] else [[(Var p), (Var q)]]
--- clausulas (Or (Not(Var p)) (Not(Var q))) = if (Not(Var p)) == (Not(Var q)) then [[(Not(Var p))]] else [[(Not(Var p)), (Not(Var q))]]
 clausulas (Or f1 f2) = if f1 == f2 then [quitadisyuncion f1] else [quitadisyuncion f1 ++ quitadisyuncion f2]
 clausulas (And f1 f2) = clausulas f1 ++ clausulas f2
 
@@ -117,11 +111,7 @@ resolucion [] [] = []
 resolucion [] c = c
 resolucion c [] = c
 resolucion [f1] [(Not f2)] = if f1 == f2 then [] else [f1, f2]
--- resolucion [f1] [f2] = [f1, f2]
 resolucion [f1] xs = hayContraria f1 xs
--- resolucion (x:xs) (y:ys) = if sonContrarias x y then xs ++ ys else resolucion xs (y:ys)
--- resolucion (x:xs) ys = if (longitud (hayContraria x ys)) < longitud ys then xs ++ hayContraria x ys else x:resolucion xs ys
--- resolucion (x:xs) ys = if (longitud (hayContraria x ys)) < longitud ys then construyeListaSinRepeticion construyeSinRepeticion (regresaCabeza (xs ++ hayContraria x ys)) xs ++ hayContraria x ys else construyeListaSinRepeticion construyeSinRepeticion (regresaCabeza (x:resolucion xs ys)) (x:resolucion xs ys)
 resolucion (x:xs) ys = if (longitud (hayContraria x ys)) < longitud ys then construyeListaSinRepeticion construyeSinRepeticion (regresaCabeza (xs ++ hayContraria x ys)) xs ++ hayContraria x ys else construyeListaSinRepeticion construyeSinRepeticion x (x:resolucion xs ys)
 
 --Función auxiliar para Ejercicio 2 (Devuelve la lista de literales sin la literal contraria del primer argumento)
@@ -136,14 +126,6 @@ sonContrarias p1 (Not p2) = if p1 == p2 then True else False
 sonContrarias (Not p1) p2 = if p1 == p2 then True else False
 sonContrarias _ _ = False
 
--- --Función auxiliar para Ejercicio 2
--- eliminaRepetidos :: (Eq a) => [a] -> [a]
--- eliminaRepetidos [] = []
--- eliminaRepetidos [b] = [b]
--- eliminaRepetidos [a,b] = if a == b then [a] else [a,b]
--- eliminaRepetidos (x:y:ys) = if x == y then eliminaRepetidos (x:ys) else x:eliminaRepetidos (y:ys)
-
-
 --Función auxiliar para Ejercicio 2
 longitud :: [a] -> Int
 longitud [] = 0
@@ -151,18 +133,18 @@ longitud (_:xs) = 1 + longitud xs
 
 ------------------------------------------------------
 
---Función auxiliar para Ejercicio 2
+--Función auxiliar para Ejercicio 2 y algoritmo de Saturación
 construyeSinRepeticion :: (Eq a) => a -> [a] -> [a]
 construyeSinRepeticion a [] = [a]
 construyeSinRepeticion a [b] = if a == b then [a] else [a,b]
 construyeSinRepeticion a (x:xs) = if elementoDe a (x:xs) == True then construyeSinRepeticion x xs else a:x:xs
 
---Función auxiliar para Ejercicio 2
+--Función auxiliar para Ejercicio 2 y algoritmo de Saturación
 construyeListaSinRepeticion :: (a -> [a] -> [a]) -> a -> [a] -> [a]
 construyeListaSinRepeticion f a [] = f a []
 construyeListaSinRepeticion f a (x:xs) = f a (construyeListaSinRepeticion f x xs)
 
---Función auxiliar para Ejercicio 2
+--Función auxiliar para Ejercicio 2 y algoritmo de Saturación
 regresaVariables :: Prop -> [String]
 regresaVariables (Cons _) = []
 regresaVariables (Var a) = [a]
@@ -172,7 +154,7 @@ regresaVariables (And a b) = regresaVariables a ++ regresaVariables b
 regresaVariables (Impl a b) = regresaVariables a ++ regresaVariables b
 regresaVariables (Syss a b) = regresaVariables a ++ regresaVariables b
 
---Función auxiliar para Ejercicio 2
+--Función auxiliar para Ejercicio 2 y para algoritmo de saturacion
 regresaCabeza :: [a] -> a
 regresaCabeza [] = error "No se puede obtener la cabeza de una lista vacía."
 regresaCabeza (x:_) = x
@@ -184,9 +166,6 @@ elementoDe x [y] = if x == y then True else False
 elementoDe y (x:xs) = if y == x then True else elementoDe y xs
 
 ---------------------------------------------------------------------------
-
-
-
 
 {-
 ALGORITMO DE SATURACION
@@ -202,36 +181,35 @@ hayResolvente (x:xs) (y:ys) = if sonContrarias x y then True else (hayResolvente
 --Ejercicio 2
 --Funcion principal que pasa la formula proposicional a fnc e invoca a res con las clausulas de la formula.
 saturacion :: Prop -> Bool
-saturacion f1 = resolvente (clausulas (fnc f1))
--- saturacion f1
---   | hayClausulaVacia (funcionR (clausulas (fnc f1))) = False
---   | funcionR (clausulas (fnc f1)) == clausulas (fnc f1) = True
---   | otherwise = saturacion (funcionR (funcionR (clausulas (fnc f1))))
+saturacion f1 = resolvente (construyeListaSinRepeticion construyeSinRepeticion (regresaCabeza (clausulas (fnc f1))) (clausulas (fnc f1)))
 
---Devuelve todos las resoluciones de la lista de clausulas recibida concatenada con la lista original
+--Funcion auxiliar Ejercicio 2. Devuelve todos las resoluciones de la lista de clausulas recibida concatenada con la lista original
 funcionR :: [Clausula] -> [Clausula]
 funcionR [] = []
 funcionR [c1] = [c1]
 funcionR [c1, c2] = if hayResolvente c1 c2 then [c1,c2] ++ [resolucion c1 c2] else [c1,c2]
--- funcionR (x:y:xs) = if hayResolvente x y then [x] ++ [resolucion x y] ++ funcionR (y:xs) else funcionR [x] ++ (y:xs)
--- funcionR xs = xs ++ [resolucion x y | x <- xs, y <- xs, x /= y, hayResolvente x y]
--- funcionR (x:xs) = (x:xs) ++ (construyeListaSinRepeticion construyeSinRepeticion x [resolucion x y | x <- (x:xs), y <- (x:xs), x /= y, hayResolvente x y])
 funcionR (x:xs) = construyeListaSinRepeticion construyeSinRepeticion x ((x:xs) ++ ([resolucion x y | x <- (x:xs), y <- (x:xs), x /= y, hayResolvente x y]))
 
+--Función auxiliar Ejercicio 2
 hayClausulaVacia :: [Clausula] -> Bool
 hayClausulaVacia [] = False
 hayClausulaVacia [a] = if a == [] then True else False 
 hayClausulaVacia (x:xs) = if x == [] then True else hayClausulaVacia xs
 
+--Función auxiliar Ejercicio 2
 resolvente :: [Clausula] -> Bool
-resolvente xs
-  | hayClausulaVacia (funcionR xs) = False
-  | funcionR xs == xs = True
-  | otherwise = resolvente (funcionR xs)
--- [[Var "p", Var "q"],[Not (Var "r"), Var "s"],[Var "t", Not (Var "u")]]
--- [[ Not (Var "p"), Var "p", Var "q" ], [ Not (Var "p"), Var "p", Var "q" ], [ Var "p", Var "q", Not (Var "p") ], [ Var "p", Var "q", Not (Var "q") ]]
+resolvente (x:xs)
+  | hayClausulaVacia (funcionR (x:xs)) = False
+  | listasIguales (funcionR xs) xs = True
+  | otherwise = resolvente (funcionR (x:xs))
 
--- [[Var "p", Var "q"], [Not (Var "p"), Var "r"]]
+--Función auxiliar Ejercicio 2
+listasIguales :: (Eq a) => [a] -> [a] -> Bool
+listasIguales [] [] = True
+listasIguales [a1] ys = if (contiene a1 ys) then True else False
+listasIguales (x:xs) ys = if (contiene x ys) == False then False else listasIguales xs ys
 
--- [[Var "p", Var "q"], [Not (Var "p"), Var "r"], [Not (Var "q"), Var "s"]]
-
+--Función auxiliar Ejercicio 2
+contiene :: (Eq a) => a -> [a] -> Bool
+contiene _ [] = False
+contiene y (x:xs) = if y == x then True else contiene y xs
